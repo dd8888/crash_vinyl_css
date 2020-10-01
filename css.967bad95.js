ebae38522fbd0970a17e98166bc8c25d7160dd59
dd8888 <daviddiazh97@gmail.com>
2020-10-01 21:41:57 +0100

    Updates

--- css.967bad95.js
+++ css.967bad95.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51330" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/damn_vinyl_css/css.967bad95.js.map
+//# sourceMappingURL=/crash_vinyl_css/css.967bad95.js.map
--- js.00a46daa.js
+++ js.00a46daa.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51330" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -324,4 +324,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/index.js"], null)
-//# sourceMappingURL=/damn_vinyl_css/js.00a46daa.js.map
+//# sourceMappingURL=/crash_vinyl_css/js.00a46daa.js.map
